473e5b3dc5bf634e5d8b6b0bced863bb3794d399
AndreaDev14 <andreatester17@gmail.com>
2022-09-26 14:05:56 -0300

    project updated

--- js/app.js
+++ js/app.js
@@ -247,13 +247,18 @@ function spinnerEffect() {
 
     refreshHTML()
     const divSpinner = document.createElement('div');
-    divSpinner.classList.add('sk-folding-cube');
+    divSpinner.classList.add('sk-cube-grid');
     divSpinner.innerHTML = `
     
-    <div class="sk-cube1 sk-cube"></div>
-  <div class="sk-cube2 sk-cube"></div>
-  <div class="sk-cube4 sk-cube"></div>
-  <div class="sk-cube3 sk-cube"></div>
+    <div class="sk-cube sk-cube1"></div>
+    <div class="sk-cube sk-cube2"></div>
+    <div class="sk-cube sk-cube3"></div>
+    <div class="sk-cube sk-cube4"></div>
+    <div class="sk-cube sk-cube5"></div>
+    <div class="sk-cube sk-cube6"></div>
+    <div class="sk-cube sk-cube7"></div>
+    <div class="sk-cube sk-cube8"></div>
+    <div class="sk-cube sk-cube9"></div>
 
     `;
     result.appendChild(divSpinner);
